3448deda0832c047fa8285469c4000c13a130423
rath-prak <rathanap@gmail.com>
2016-08-24 17:28:12 +1200

    add pulsing button

--- js/main.js
+++ js/main.js
@@ -89,6 +89,44 @@ $(document).ready(function(){
           }
     };
 
+  /**
+  * PULSE ANIMATION
+  */
+
+  $(document).on('click', '.pulse', function(){
+    $.fn.fullpage.moveSectionDown();
+  });
+
+  TweenMax.to('.pulse-button circle:nth-child(1)', 2, {
+    attr: {
+      r: 25,
+      "stroke-width": 1,
+      "stroke-opacity": 1
+    },
+    opacity: 0,
+    repeat: -1,
+    delay: 1
+    });
+
+  TweenMax.to('.pulse-button circle:nth-child(2)', 2, {
+    attr: {
+      r: 25,
+      "stroke-width": 1,
+      "stroke-opacity": 1
+    },
+    opacity: 0,
+    repeat: -1
+    });
+
+  TweenMax.to('.pulse-button circle:nth-child(3)', 1.5, {
+    attr: {
+      r: 2
+    },
+    repeat: -1,
+    yoyo: true,
+    ease: Power2.easeOut
+  });
+
 
 
 }); // end of document.ready()
