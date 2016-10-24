7ad23d1276c1c0cd386fa8a18f3998eaa55e5d4c
rath-prak <rathanap@gmail.com>
2016-10-24 17:06:25 +1300

    refactor nativation function  main.js

--- js/main.js
+++ js/main.js
@@ -17,40 +17,48 @@ $(document).ready(function(){
 
 //NAVIGATION
 
-// var navigationSlideMenu = {
-//   init: function() {
-//     this.cacheDom();
-//   },
-//   cacheDom: function() {
-//     this.
-//   }
-// }
+(function() {
+  var navigationSlideMenu = {
+    init: function() {
+      this.cacheDom();
+      this.render();
+    },
+    cacheDom: function() {
+      this.$menuBar = $(".navbar-toggle");
+      this.$menuListItem = $("menu-list-items");
+      this.$slideDown = $(".fp-nav-wrapper");
+      this.$navBackButton = $(".nav-back-button");
+    },
+    render: function () {
+      var menuToggle = new TimelineMax({ paused:true, reversed:true });
+
+      TweenMax.set(this.$slideDown, { 
+        y: '-100%' 
+      });
+
+      menuToggle.to(this.$slideDown, 1, { 
+        y: '0%',
+        ease: Power4.easeInOut,
+      });
+
+      $('.navbar-toggle, .menu-list-items, .nav-back-button').click(function () {
+        menuToggle.reversed() ? menuToggle.restart() : menuToggle.reverse();
+      });
+      // (this.$menuBar).add(this.$menuListItem).add(this.$navBackButton).click(function () {
+      // menuToggle.reversed() ? menuToggle.restart() : menuToggle.reverse();
+      // });
+    },
+  };
+  navigationSlideMenu.init();
+})()
 
 
 
 
 
 
-var $menuBar = $(".navbar-toggle");
-var $menuListItem = $("menu-list-items");
-var $slideDown = $(".fp-nav-wrapper");
-var $navBackButton = $(".nav-back-button")
-var menuToggle = new TimelineMax({paused:true, reversed:true});
 
 
-TweenMax.set($slideDown, {
-    y: '-100%'
-  });
-
-menuToggle  
-  .to($slideDown, 1, {
-  y: '0%',
-  ease: Power4.easeInOut
-  });
-
-$menuBar.add($menuListItem).add($navBackButton).click(function () {
-  menuToggle.reversed() ? menuToggle.restart() : menuToggle.reverse();
-});
 
 
 // hamburger menu
