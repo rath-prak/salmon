b80aa63968c1b02f3e34e42acb1d1971e354eb30
rath-prak <rathanap@gmail.com>
2016-11-21 21:27:22 +1300

    change main.js add hide navigation when scrolling down

--- js/main.js
+++ js/main.js
@@ -14,9 +14,43 @@ $(document).ready(function(){
 // PRE-LAODER
   $(window).load(function(){
     // pagePreloader()
-    setTimeout(pagePreloader, 800);
+    setTimeout(pagePreloader, 500);
   });
 
+// HIDE NAV MENU ON SCROLLING
+  var NavNide = function () {
+    var previousScroll = 0;
+
+    $(window).scroll(function () {
+      var $currentScroll = $(this).scrollTop();
+      var $logoAndHamburger = $('.logo, .navbar-toggle');
+      var $secondaryNav = $('.secondary-nav');
+
+      if ($currentScroll > previousScroll){
+        TweenMax.to($secondaryNav, 0.2, {
+        y: '-100',
+        ease: Linear.easeNone
+        })
+
+      TweenMax.to($secondaryNav, 0.2, {
+        css: {background: "#072231"},
+        ease: Linear.easeNone
+        })
+      }
+      else {
+        function slideBack (divElement, yPos) {
+            TweenMax.to(divElement, 0.2, {
+            y: yPos,
+            ease: Linear.easeNone
+            });
+          }
+        slideBack($logoAndHamburger, 30);
+        slideBack($secondaryNav, 0);
+        }
+        previousScroll = $currentScroll;
+    });
+  }
+  NavNide(); 
 
 // NAVIGATION
 navigationSlideMenu.init();
