ea100a44e72a7b3eaad047c79cf1a1e81981df32
rath-prak <rathanap@gmail.com>
2016-11-21 21:38:16 +1300

    fix merge conflicks

--- js/lib/pixiBackground.js
+++ js/lib/pixiBackground.js
@@ -1,48 +1,34 @@
-// var pixiBackground = function () {
+var pixiBackground = function () {
 
-//   var viewWidth = 630;
-//   var viewHeight = 410;
+  var viewWidth = 630;
+  var viewHeight = 410;
 
 
-//   var renderer = PIXI.autoDetectRenderer(viewWidth, viewHeight);
-//   renderer.view.className = "rendererView";
-//   // var renderer = PIXI.autoDetectRenderer(800, 600);
-//   document.body.appendChild(renderer.view);
+  var renderer = PIXI.autoDetectRenderer(viewWidth, viewHeight);
+  renderer.view.className = "rendererView";
+  // var renderer = PIXI.autoDetectRenderer(800, 600);
+  document.body.appendChild(renderer.view);
 
-//   // create the root of the scene graph
-//   var stage = new PIXI.Container();
+  // create the root of the scene graph
+  var stage = new PIXI.Container();
 
-//   var count = 0;
+  var count = 0;
 
-//   // build a rope!
-//   var ropeLength = 918 / 7;
+  // build a rope!
+  var ropeLength = 918 / 7;
 
-//   var points = [];
+  var points = [];
 
-//   for (var i = 0; i < 20; i++)
-//   {
-//       points.push(new PIXI.Point(i * ropeLength, 0));
-//   }
+  for (var i = 0; i < 20; i++)
+  {
+      points.push(new PIXI.Point(i * ropeLength, 0));
+  }
 
-<<<<<<< HEAD:js/lib/background.js
-//   var strip = new PIXI.mesh.Rope(PIXI.Texture.fromImage('../img/underwaterocean.png'), points);
-=======
   var strip = new PIXI.mesh.Rope(PIXI.Texture.fromImage('img/pixi/underwaterocean.png'), points);
->>>>>>> f9bf67593d9a35bd669adf2ef6e2e7e9cab7b5ff:js/lib/pixiBackground.js
 
-//   strip.x = -750;
-//   strip.y = -200;
+  strip.x = -750;
+  strip.y = -200;
 
-<<<<<<< HEAD:js/lib/background.js
-//   var snakeContainer = new PIXI.Container();
-//   snakeContainer.position.x = 400;
-//   snakeContainer.position.y = 300;
-
-//   snakeContainer.scale.set(0.70);
-//   stage.addChild(snakeContainer);
-
-//   snakeContainer.addChild(strip);
-=======
   var backgroundContainer = new PIXI.Container();
   backgroundContainer.position.x = 400;
   backgroundContainer.position.y = 300;
@@ -51,34 +37,28 @@
   stage.addChild(backgroundContainer);
 
  backgroundContainer.addChild(strip);
->>>>>>> f9bf67593d9a35bd669adf2ef6e2e7e9cab7b5ff:js/lib/pixiBackground.js
 
-//   // start animating
-//   requestAnimationFrame(animate);
+  // start animating
+  requestAnimationFrame(animate);
 
-//   function animate() {
+  function animate() {
 
-//       count += 0.01;
+      count += 0.01;
 
-<<<<<<< HEAD:js/lib/background.js
-//       // make the snake
-//       for (var i = 0; i < points.length; i++) {
-=======
       for (var i = 0; i < points.length; i++) {
->>>>>>> f9bf67593d9a35bd669adf2ef6e2e7e9cab7b5ff:js/lib/pixiBackground.js
 
-//           points[i].y = Math.sin((i * 0.5) + count) * 30;
+          points[i].y = Math.sin((i * 0.5) + count) * 30;
 
-//           points[i].x = i * ropeLength + Math.cos((i * 0.3) + count) * 5;
+          points[i].x = i * ropeLength + Math.cos((i * 0.3) + count) * 5;
 
-//       }
+      }
 
-//       // render the stage
-//       renderer.render(stage);
+      // render the stage
+      renderer.render(stage);
 
-//       requestAnimationFrame(animate);
-//   }
+      requestAnimationFrame(animate);
+  }
 
-// }
+}
 
-// module.exports = pixiBackground
+module.exports = pixiBackground
