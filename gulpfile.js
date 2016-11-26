6e7dd376744f7b62a741f2d4eb4f0d6858186e52
rath-prak <rathanap@gmail.com>
2016-11-26 16:45:13 +1300

    minify css

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var browserSync = require('browser-sync').create();
 var autoprefixer = require('gulp-autoprefixer');
 var browserify = require('gulp-browserify');
 var uglify = require('gulp-uglify');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 
 gulp.task('sass', function() {
 	return gulp.src('css/sass/**/*.scss')
@@ -30,10 +32,21 @@ gulp.task('browserify', function() {
 	.pipe(gulp.dest('./js/dist'))
 });
 
-gulp.task('watch', ['browserSync', 'sass', 'browserify'], function() {
+gulp.task('cssmin', function () {
+  gulp.src('./css/main.css')
+      .pipe(cssmin())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', ['browserSync', 'sass', 'browserify', 'cssmin'], function() {
 	gulp.watch('css/sass/**/*.scss', ['sass']);
 	// reload browser when HTML or JS file is changed
 	gulp.watch('./*.html', browserSync.reload);
 	gulp.watch('js/**/*.js', browserSync.reload);
 	gulp.watch('js/dist/*.js', browserSync.reload);
 });
+
+
+ 
+
