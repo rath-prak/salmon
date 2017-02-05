7384d314997a5d3f53a13eda1750a6a2e67fa7bd
rath-prak <rathanap@gmail.com>
2017-02-05 16:08:29 +1300

    fix footer margins

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var browserSync = require('browser-sync').create();
 var autoprefixer = require('gulp-autoprefixer');
 var browserify = require('gulp-browserify');
 var uglify = require('gulp-uglify');
-var cssmin = require('gulp-cssmin');
+// var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
 
 gulp.task('sass', function() {
@@ -32,21 +32,19 @@ gulp.task('browserify', function() {
 	.pipe(gulp.dest('./js/dist'))
 });
 
-gulp.task('cssmin', function () {
-  gulp.src('./css/main.css')
-      .pipe(cssmin())
-      .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest('./css'));
-});
+// gulp.task('cssmin', function () {
+//   gulp.src('./css/main.css')
+//       .pipe(cssmin())
+//       .pipe(rename({suffix: '.min'}))
+//       .pipe(gulp.dest('./css'));
+// });
 
-gulp.task('watch', ['browserSync', 'sass', 'browserify', 'cssmin'], function() {
+// dont forget to 'cssmin' for production
+
+gulp.task('watch', ['browserSync', 'sass', 'browserify',], function() {
 	gulp.watch('css/sass/**/*.scss', ['sass']);
 	// reload browser when HTML or JS file is changed
 	gulp.watch('./*.html', browserSync.reload);
 	gulp.watch('js/**/*.js', browserSync.reload);
 	gulp.watch('js/dist/*.js', browserSync.reload);
 });
-
-
- 
-
